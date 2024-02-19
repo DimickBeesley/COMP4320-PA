4c4e82d84b1bb6d92c456e23b01b7c33094b15a7
DimickBeesley <dbeesley8@gmail.com>
2024-02-19 14:18:21 -0600

    I let VSCode refactor for Assignment1 package

--- Assignment1/MathOpEncoderBin.java
+++ Assignment1/MathOpEncoderBin.java
@@ -1,7 +1,12 @@
+package Assignment1;
+
 import java.io.*;  // for ByteArrayOutputStream and DataOutputStream
 
-//TODO: Make this thing actually encode our shit
-public class MathOpEncoderBin implements MathOpEncoder /* TODO: Do we need the equivalent of this? FriendBinConst*/ {
+public class MathOpEncoderBin implements MathOpEncoder {
+
+  private static String DEFAULT_ENCODING = "UTF-16BE";
+  private static int MAX_OP_NAME_LEN = 255;
+
 
   private String encoding;  // Character encoding
 
@@ -13,26 +18,21 @@ public class MathOpEncoderBin implements MathOpEncoder /* TODO: Do we need the e
     this.encoding = encoding;
   }
 
-  public byte[] encode(Friend friend) throws Exception {
+  public byte[] encode(MathOp mathOp) throws Exception {
 
     ByteArrayOutputStream buf = new ByteArrayOutputStream();
     DataOutputStream out = new DataOutputStream(buf);
-    out.writeLong(friend.ID);
-    // Will deal with the lasname at the end
-    out.writeShort(friend.streetNumber);
-    out.writeInt(friend.zipCode);
-    byte flags = 0;
-    if (friend.single)
-	flags = SINGLE_FLAG;
-    if (friend.rich)
-	flags |= RICH_FLAG;
-    if (friend.female)
-	flags |= FEMALE_FLAG;
-    out.writeByte(flags);
 
-    byte[] encodedLastname = friend.lastName.getBytes(encoding);
-    if (encodedLastname.length > MAX_LASTNAME_LEN)
-      throw new IOException("Friend lastname exceeds encoded length limit");
+    out.writeInt(mathOp.tml);
+    out.writeInt(mathOp.opCode);
+    out.writeInt(mathOp.operand1);
+    out.writeInt(mathOp.operand2);
+    out.writeInt(mathOp.requestID);
+    out.writeInt(mathOp.opNameLength);
+
+    byte[] encodedLastname = mathOp.opName.getBytes(encoding);
+    if (encodedLastname.length > MAX_OP_NAME_LEN)
+      throw new IOException("mathOp lastname exceeds encoded length limit");
     out.writeByte(encodedLastname.length); // provides length of lastname
     out.write(encodedLastname);
     out.flush();
